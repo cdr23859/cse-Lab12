c3ca7a2eb208bac2b9dde409e9560d61f606215a
Mark Hernandez <MH81796@gmail.com>
2018-04-04 11:42:51 -0500

    Improve readability and ease of solving

--- Lab12-Recursion/src/unl/cse/recursion/Fibonacci.java
+++ Lab12-Recursion/src/unl/cse/recursion/Fibonacci.java
@@ -1,18 +1,21 @@
 package unl.cse.recursion;
 
 public class Fibonacci {
-	
-	public int fibonacci(int n) {
-		if(n == 0) 
-			return 0;
-		else if(n == 1) 
-			return 1;
-		else
-			return fibonacci(n-1) + fibonacci(n-2);
+	public Fibonacci () {
+		// In case you need to initialize a private variable
 	}
-	
+
+	public int fibonacci(int n) {
+		if(n == 0) {
+			return 0;
+		} else if(n == 1) { 
+			return 1;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
+
 	public static void main(String args[]) {
-		
 		Fibonacci f = new Fibonacci();
 		int result, n = 45;
 
@@ -20,11 +23,10 @@ public class Fibonacci {
 		start = System.nanoTime();
 		result = f.fibonacci(n);
 		end = System.nanoTime();
-		
-		double ms = (end-start) / 1000000.0;
-		
-		System.out.println("fibonacci("+n+") = "+result);
-		System.out.println("execution time = "+ms+"ms");
-		
+
+		double ms = (end - start) / 1000000.0;
+
+		System.out.println("fibonacci(" + n + ") = " + result);
+		System.out.println("execution time = " + ms + "ms");
 	}
 }
